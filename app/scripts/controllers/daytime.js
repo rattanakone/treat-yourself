0bf9c203a7d021f0e7e62c07ebf87d678fc78a2b
rattanakone <rattanakone@users.noreply.github.com>
2016-02-09 15:18:16 -0800

    added videos and hiding theme in daytime view

--- app/scripts/controllers/daytime.js
+++ app/scripts/controllers/daytime.js
@@ -9,11 +9,20 @@
  */
 angular.module('treatYourselfApp')
   .controller('DaytimeCtrl', function ($scope, $sce) {
-    $scope.videos = 'oHg5SJYRHA0';
-    $scope.hairVideo = 'AR8D2yqgQ1U';
-    var embedCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.videos + '" frameborder="0" allowfullscreen></iframe>';
-    $scope.currentVideo = $sce.trustAsHtml(embedCode);
-    var hairCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.hairVideo + '" frameborder="0" allowfullscreen></iframe>';
-    $scope.testVideo = $sce.trustAsHtml(hairCode);
-    console.log("set current video to: " + $scope.currentVideo);
+    $scope.firstMakeupVideoId = '9OFpfTd0EIs';
+    $scope.firstHairVideoId = 'AR8D2yqgQ1U';
+    $scope.firstOutfitVideoId = 'ZZyGDiUnta4';
+    var firstMakeupEmbedCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.firstMakeupVideoId + '" frameborder="0" allowfullscreen></iframe>';
+    $scope.firstMakeupVideo = $sce.trustAsHtml(firstMakeupEmbedCode);
+    var firstHairEmbedCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.firstHairVideoId + '" frameborder="0" allowfullscreen></iframe>';
+    $scope.firstHairVideo = $sce.trustAsHtml(firstHairEmbedCode);
+    var firstOutfitEmbedCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.firstOutfitVideoId + '" frameborder="0" allowfullscreen></iframe>';
+   	$scope.firstOutfitVideo = $sce.trustAsHtml(firstOutfitEmbedCode);
+
+    $scope.themeOne = false;
+    $scope.themeTwo = false;
+    $scope.themeThree = false;
+    $scope.themeFour = false;
+    $scope.themeFive = false;
+
   });
