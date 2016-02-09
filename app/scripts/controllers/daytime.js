9be48237866b9e8ef308db6306a4383ccafd09be
rattanakone <rattanakone@users.noreply.github.com>
2016-02-08 18:37:20 -0800

    embedded youtube videos in daytime looks

--- app/scripts/controllers/daytime.js
+++ app/scripts/controllers/daytime.js
@@ -8,10 +8,12 @@
  * Controller of the treatYourselfApp
  */
 angular.module('treatYourselfApp')
-  .controller('DaytimeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('DaytimeCtrl', function ($scope, $sce) {
+    $scope.videos = 'oHg5SJYRHA0';
+    $scope.hairVideo = 'AR8D2yqgQ1U';
+    var embedCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.videos + '" frameborder="0" allowfullscreen></iframe>';
+    $scope.currentVideo = $sce.trustAsHtml(embedCode);
+    var hairCode = '<iframe ng-if="currentVideo" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.hairVideo + '" frameborder="0" allowfullscreen></iframe>';
+    $scope.testVideo = $sce.trustAsHtml(hairCode);
+    console.log("set current video to: " + $scope.currentVideo);
   });
