74d3776c60688c8fcd6e27ab9b4080dfcbea2913
rattanakone <rattanakone@users.noreply.github.com>
2016-02-28 23:27:47 -0800

    somewhat finished mix view

--- app/scripts/controllers/mix.js
+++ app/scripts/controllers/mix.js
@@ -21,7 +21,7 @@ angular.module('treatYourselfApp')
 
     $scope.randomMakeupVideo = function () {
     	var randomMakeupIndex = Math.floor(Math.random()*50) +1;
-    	var embedMakeupCode = '<iframe ng-if="makeupVideoEmbed" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.makeUpVideos.items[randomMakeupIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
+    	var embedMakeupCode = '<iframe ng-if="makeupVideoEmbed" width="420" height="315" src="https://www.youtube.com/embed/' + $scope.makeUpVideos.items[randomMakeupIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
     	$scope.makeupVideoEmbed = $sce.trustAsHtml(embedMakeupCode); 
         console.log("set current video to: " + $scope.makeupVideoEmbed); 
         console.log($scope.makeUpVideos.items[randomMakeupIndex].snippet.resourceId.videoId);
@@ -30,7 +30,7 @@ angular.module('treatYourselfApp')
 
     $scope.randomHairVideo = function () {
         var randomHairIndex = Math.floor(Math.random()*50) +1;
-        var embedHairCode = '<iframe ng-if="hairVideoEmbed" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.hairVideos.items[randomHairIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
+        var embedHairCode = '<iframe ng-if="hairVideoEmbed" width="420" height="315" src="https://www.youtube.com/embed/' + $scope.hairVideos.items[randomHairIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
         $scope.hairVideoEmbed = $sce.trustAsHtml(embedHairCode); 
         console.log("set current video to: " + $scope.hairVideoEmbed); 
         console.log($scope.hairVideos.items[randomHairIndex].snippet.resourceId.videoId);
@@ -39,7 +39,7 @@ angular.module('treatYourselfApp')
 
     $scope.randomOutfitVideo = function () {
         var randomOutfitIndex = Math.floor(Math.random()*50) +1;
-        var embedOutfitCode = '<iframe ng-if="outfitVideoEmbed" width="580" height="350" src="https://www.youtube.com/embed/' + $scope.outfitVideos.items[randomOutfitIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
+        var embedOutfitCode = '<iframe ng-if="outfitVideoEmbed" width="420" height="315" src="https://www.youtube.com/embed/' + $scope.outfitVideos.items[randomOutfitIndex].snippet.resourceId.videoId + '" frameborder="0" allowfullscreen></iframe>'; 
         $scope.outfitVideoEmbed = $sce.trustAsHtml(embedOutfitCode); 
         console.log("set current video to: " + $scope.outfitVideoEmbed);
         console.log($scope.outfitVideos.items[randomOutfitIndex.snippet.resourceId.videoId]);
